acd3635c4c286f5319c42ec022163455abdb5dc4
Abdul Rahman Arraisi <itsarraisi@gmail.com>
2018-08-05 11:08:45 +0700

    first commit format-string.java

---
+++ FormatString.java
@@ -0,0 +1,28 @@
+public class FormatString
+{
+    public static void main (String[] args) {
+        String S = "00-44  48 5555 8361" ;
+        // want to format it as "004-448-555-583-61"
+        S = S.replace("-", "");
+        S = S.replace(" ", "");
+        System.out.println(S);
+
+        char[] caddyString = S.toCharArray();
+        String result = "";
+
+        Integer i = 1 ;
+        Integer resultLength = S.length() + (S.length()/3);
+
+
+        for (char caddyFor : caddyString) {
+            if (i%3 == 0 && i != S.length()) {
+                result = result + caddyFor + "-";
+            } else {
+                result = result + caddyFor;
+            }
+            i++;
+        }
+
+        System.out.println("Index : " + i + " value " + result);
+    }
+}
